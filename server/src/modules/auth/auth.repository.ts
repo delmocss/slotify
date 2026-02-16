fe89617472edf7406c5f4eeab2e673a7e6c60490
delmocs <delmoralcs@gmail.com>
2026-02-16 19:10:22 +0100

    feat: generate and store business slug during registration

--- server/src/modules/auth/auth.repository.ts
+++ server/src/modules/auth/auth.repository.ts
@@ -1,35 +1,40 @@
 import { pool } from "../../config/db"
 
 export class AuthRepository {
-  async findByEmail(email: string) {
-    const result = await pool.query(
-      "SELECT * FROM businesses WHERE email = $1",
-      [email]
-    )
-    return result.rows[0]
-  }
+    async findByEmail(email: string) {
+        const result = await pool.query(
+            "SELECT * FROM businesses WHERE email = $1",
+            [email]
+        )
+        return result.rows[0]
+    }
 
-  async create(data: {
-    name: string
-    email: string
-    password_hash: string
-  }) {
-    const result = await pool.query(
-      `INSERT INTO businesses (name, email, password_hash)
-       VALUES ($1, $2, $3)
-       RETURNING id, name, email`,
-      [data.name, data.email, data.password_hash]
-    )
+    async create(data: {
+        name: string
+        email: string
+        password_hash: string
+    }) {
+        const slug = data.name
+            .toLowerCase()
+            .replace(/\s+/g, "-")
 
-    return result.rows[0]
-  }
+        const result = await pool.query(
+            `INSERT INTO businesses (name, email, password_hash, slug)
+     VALUES ($1, $2, $3, $4)
+     RETURNING id, name, email, slug`,
+            [data.name, data.email, data.password_hash, slug]
+        )
 
-  async findById(id: string) {
-  const result = await pool.query(
-    "SELECT id, name, email FROM businesses WHERE id = $1",
-    [id]
-  )
-  return result.rows[0]
-}
+        return result.rows[0]
+    }
+
+
+    async findById(id: string) {
+        const result = await pool.query(
+            "SELECT id, name, email FROM businesses WHERE id = $1",
+            [id]
+        )
+        return result.rows[0]
+    }
 
 }
